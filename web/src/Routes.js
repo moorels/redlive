a10ea734884f175429e50dc5ff6e73d02224d1eb
moorels <sales@micropls.com>
2021-09-04 21:11:14 +1000

    new1

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,32 +1,42 @@
 import HeadzLayout from './layouts/HeadzLayout/HeadzLayout'
-
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route, Private } from '@redwoodjs/router'
 import ContactsLayout from 'src/layouts/ContactsLayout'
 import VaultsLayout from 'src/layouts/VaultsLayout'
+import { useState } from 'react'
+import { UserContext } from './UserContext'
+
+const sum = Math.floor(Math.random() * 9999999)
 
 const Routes = () => {
+  const [user, setUser] = useState('test')
+
   return (
     <Router>
-      <Set wrap={HeadzLayout}>
-        <Set wrap={ContactsLayout}>
-          <Route path="/contacts/new" page={ContactNewContactPage} name="newContact" />
-          <Route path="/contacts/{id:Int}/edit" page={ContactEditContactPage} name="editContact" />
-          <Route path="/contacts/{id:Int}" page={ContactContactPage} name="contact" />
-          <Route path="/contacts" page={ContactContactsPage} name="contacts" />
-        </Set>
-        <Set wrap={VaultsLayout}>
-          <Route path="/vaults/new" page={VaultNewVaultPage} name="newVault" />
-          <Route path="/vaults/{id:Int}/edit" page={VaultEditVaultPage} name="editVault" />
-          <Route path="/vaults/{id:Int}" page={VaultVaultPage} name="vault" />
-          <Route path="/vaults" page={VaultVaultsPage} name="vaults" />
-        </Set>
-        <Route path="/about" page={AboutPage} name="about" />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Set wrap={HeadzLayout}>
+          <Set wrap={ContactsLayout}>
+            <Route path="/contacts/new" page={ContactNewContactPage} name="newContact" />
+            <Route path="/contacts/{id:Int}/edit" page={ContactEditContactPage} name="editContact" />
+            <Route path="/contacts/{id:Int}" page={ContactContactPage} name="contact" />
+            <Route path="/contacts" page={ContactContactsPage} name="contacts" />
+          </Set>
+          <Private unauthenticated="login">
+            <Set wrap={VaultsLayout}>
+              <Route path={`/vaults/${sum}/new`} page={VaultNewVaultPage} name="newVault" />
+              <Route path={`/vaults/${sum}/{id:Int}/edit`} page={VaultEditVaultPage} name="editVault" />
+              <Route path={`/vaults/${sum}/{id:Int}`} page={VaultVaultPage} name="vault" />
+              <Route path="/vaults" page={VaultVaultsPage} name="vaults" />
+            </Set>
+          </Private>
 
-        <Route path="/" page={HomePage} name="home" />
-        <Route path="/login" page={LoginPage} name="login" />
-        <Route path="/signup" page={SignupPage} name="signup" />
-        <Route notfound page={NotFoundPage} />
-      </Set>
+          <Route path="/about" page={AboutPage} name="about" />
+
+          <Route path="/" page={HomePage} name="home" />
+          <Route path="/login" page={LoginPage} name="login" />
+          <Route path="/signup" page={SignupPage} name="signup" />
+          <Route notfound page={NotFoundPage} />
+        </Set>
+      </UserContext.Provider>
     </Router>
   )
 }
